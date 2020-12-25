7a7d0b55881311cacf48288dbea84dc67038f0e6
IraCannet <58562850+IraPapsheva@users.noreply.github.com>
2020-12-25 23:18:46 +0100

    axios-integration

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from 'axios';
+ import Loader from 'react-loader-spinner';
+
+export default function Weather(props){
+function handleResponse(response){
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}°C`)
+}
+    let apiKey = "c4ec87540b7d8d71553d9dad178f26b1";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+    return(
+        <Loader
+         type="Puff"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={3000} //3 secs
+ 
+      />
+    )
+}
